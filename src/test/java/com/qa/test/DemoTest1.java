239a32320bc41835e8fa95de1011269df6f045a1
bhagyashripasarkar90 <bhagyashripasarkar90@gmail.com>
2020-10-18 22:46:32 +0530

    Added maven demo

---
+++ src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,39 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+		int a = 10,b = 20;
+		Assert.assertEquals(30, a+b);
+		
+	}
+
+	@Test
+	public void sub() {
+		System.out.println("sub");
+		int a = 10,b = 20;
+		Assert.assertEquals(10, b-a);
+		
+	}
+	@Test
+	public void div() {
+		System.out.println("div");
+		int a = 10,b = 20;
+		Assert.assertEquals(2, b/a);
+		
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("mul");
+		int a = 10,b = 20;
+		Assert.assertEquals(200, a*b);
+		
+	}
+
+
+}
